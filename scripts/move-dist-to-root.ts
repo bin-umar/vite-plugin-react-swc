0b6d85d92694a4200089e6b5b75f857f505fa7ae
Jahongir Tulforov <jakha_96-96@mail.ru>
2024-12-22 18:32:05 +0100

    Update move-dist-to-root.ts

--- scripts/move-dist-to-root.ts
+++ scripts/move-dist-to-root.ts
@@ -1,5 +1,10 @@
 import { promises as fs } from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Get the directory name
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const distDir = path.resolve(__dirname, '../dist');
 const rootDir = path.resolve(__dirname, '..');
